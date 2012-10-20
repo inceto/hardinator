1181d024e993d417eb70004df486ee357dd00af1
Reto Bachmann-Gmür <reto@apache.org>
2012-10-20 19:57:04 +0200

    introduced Paragraph class. Moved console output to main

---
+++ src/main/java/ch/unibe/scs/into/Paragraph.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.unibe.scs.into;
+
+/**
+ *
+ * @author reto
+ */
+public class Paragraph {
+	private final String text;
+
+	Paragraph(String text) {
+		this.text = text;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+}
