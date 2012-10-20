0e8d3325598049802c2ad2e5890976979acc7b94
Reto Bachmann-Gmür <reto@apache.org>
2012-10-20 20:12:33 +0200

    some basic hardness evaluation infrastructure

--- src/main/java/ch/unibe/scs/into/Paragraph.java
+++ src/main/java/ch/unibe/scs/into/Paragraph.java
@@ -6,9 +6,8 @@ package ch.unibe.scs.into;
 
 /**
  *
- * @author reto
  */
-public class Paragraph {
+public class Paragraph implements Comparable<Paragraph> {
 	private final String text;
 
 	Paragraph(String text) {
@@ -19,4 +18,34 @@ public class Paragraph {
 		return text;
 	}
 	
+	public int getComprehensionCosts() {
+		int totalCosts = 0;
+		for (HardnessEvaluator he :Configuration.getHardnessEvaluators()) {
+			totalCosts += he.getComprehensionCosts(this);
+		}
+		return totalCosts;
+	}
+
+	public int compareTo(Paragraph other) {
+		int costsHere = getComprehensionCosts();
+		int costsThere = other.getComprehensionCosts();
+		if (costsHere == costsThere) {
+			return getText().compareTo(other.getText());
+		}
+		return costsHere - costsThere;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (!other.getClass().equals(this.getClass())) {
+			return false;
+		}
+		return getText().equals(((Paragraph)other).getText());
+	}
+	
+	@Override
+	public int hashCode() {
+		return getText().hashCode();
+	}
 }
+
