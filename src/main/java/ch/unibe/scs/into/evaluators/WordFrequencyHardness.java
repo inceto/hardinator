b21c3d3ccda63d292aec40723509d4dae377f424
incidincer <incidincer@device-d4fa4f>
2012-11-13 11:16:15 +0100

    add a skeleton

---
+++ src/main/java/ch/unibe/scs/into/evaluators/WordFrequencyHardness.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.unibe.scs.into.evaluators;
+
+import ch.unibe.scs.into.HardnessEvaluator;
+import ch.unibe.scs.into.Paragraph;
+
+
+public class WordFrequencyHardness implements HardnessEvaluator{
+
+	public int getComprehensionCosts(Paragraph paragraph) {
+		//TODO implement
+		return 0;
+	}
+	
+}
