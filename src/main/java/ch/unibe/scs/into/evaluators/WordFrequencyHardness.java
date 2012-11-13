5dfc56fc4e5f93531dbd05641da653f395d85eff
incidincer <incidincer@device-d4fa4f>
2012-11-13 11:56:10 +0100

    changes-2

--- src/main/java/ch/unibe/scs/into/evaluators/WordFrequencyHardness.java
+++ src/main/java/ch/unibe/scs/into/evaluators/WordFrequencyHardness.java
@@ -4,6 +4,11 @@
  */
 package ch.unibe.scs.into.evaluators;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 import ch.unibe.scs.into.HardnessEvaluator;
 import ch.unibe.scs.into.Paragraph;
 
@@ -15,4 +20,25 @@ public class WordFrequencyHardness implements HardnessEvaluator{
 		return 0;
 	}
 	
+	public static void main (String[] args) throws IOException
+	{
+		//System.out.println("wordfrequency.txt");
+		Reader reader = new InputStreamReader(
+				WordFrequencyHardness.class.getResourceAsStream("wordfrequency.txt"));
+		BufferedReader in = new BufferedReader(reader);
+		String line = in.readLine();
+		while ( line != null)
+		{
+			String[] parts = line.split(" ");
+			System.out.println(parts[0]);
+			System.out.println(parts[1]);
+			
+			System.out.println(line);
+			line = in.readLine();
+			
+			
+		}
+		in.close();
+		
+	}
 }
